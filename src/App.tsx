e2162dce01efc3d5c6e5e8a7e734d3e471d97d60
nitin-dighore <nitin@devbeeit.com>
2022-12-25 20:38:36 +0530

    more upgradation.

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,11 @@ function App() {
   const identifyImageRef: any = useRef();
 
   // useStates
+  const [isModuleLoading, setIsModuleLoading] = useState(false);
   const [className, setClassName] = useState("");
-  const [images, setImages] = useState<any>([]);
-  const [imagesUrls, setImagesUrls] = useState<any>([]);
-  const [toIdentifyImage, setToIdentifyImages] = useState<any>([]);
-  const [toIdentifyImageUrls, setToIdentifyImagesUrls] = useState<any>([]);
+  const [selectedExampleImage, setSelectedExampleImage] = useState<any>(null);
+  const [exampleImagesUrls, setExampleImagesUrls] = useState<any>([]);
+  const [selectedIdentifyImage, setSelectedIdentifyImage] = useState<any>(null);
   const [predication, setPredication] = useState<any>(null);
   const [classifier, setClassifier] = useState<any>(null);
   const [mobilenet, setMobilenet] = useState<any>(null);
@@ -42,9 +42,12 @@ function App() {
     // Load mobilenet.
     const mobilenetInstance = await mobilenetModule.load();
     setMobilenet(mobilenetInstance);
+
+    setIsModuleLoading(false);
   };
 
   useEffect(() => {
+    setIsModuleLoading(true);
     loadModules();
   }, []);
 
@@ -57,150 +60,236 @@ function App() {
   const uploadImage = (e: any) => {
     const { files } = e.target;
 
-    if (files.length > 0) {
-      const imagesArray = Object.values(files);
+    if (files) {
+      const imageObject = files[0];
 
-      setImages(imagesArray);
+      const exampleImageUrl = URL.createObjectURL(imageObject);
 
-      const imagesUrls = imagesArray.map((image: any) => {
-        const url = URL.createObjectURL(image);
-        return { img: url };
+      setSelectedExampleImage({
+        img: exampleImageUrl,
+        class: className,
       });
-
-      setImagesUrls(imagesUrls);
     }
   };
 
   const createExamples = async () => {
-    console.log(className);
-
     const imageInPixelForm = tf.browser.fromPixels(exampleImageRef.current);
     const inferredImage = mobilenet.infer(imageInPixelForm, className);
     classifier.addExample(inferredImage, className);
+
+    setExampleImagesUrls([
+      ...exampleImagesUrls,
+      {
+        ...selectedExampleImage,
+      },
+    ]);
+
+    setSelectedExampleImage(null);
   };
 
   // Uploading Image for identification
   const uploadImageIdentification = (e: any) => {
     const { files } = e.target;
 
-    if (files.length > 0) {
-      const imagesArray = Object.values(files);
+    if (files) {
+      const imagesObject = files[0];
 
-      setToIdentifyImages(imagesArray);
+      const toIdentifyImageUrl = URL.createObjectURL(imagesObject);
 
-      const imagesUrls = imagesArray.map((image: any) => {
-        const url = URL.createObjectURL(image);
-        return { img: url };
+      setSelectedIdentifyImage({
+        img: toIdentifyImageUrl,
+        class: className,
       });
-
-      setToIdentifyImagesUrls(imagesUrls);
     }
   };
 
   const verifyImage = async () => {
+    setPredication(null);
     const imageInPixelForm = tf.browser.fromPixels(identifyImageRef.current);
     const inferredImage = mobilenet.infer(imageInPixelForm, true);
-    console.log("Predictions:");
     const classPredication = await classifier.predictClass(inferredImage);
-    console.log(classPredication);
 
-    setPredication(classPredication);
+    const confidenceValuesArray: any = Object.values(
+      classPredication.confidences
+    );
+
+    let maximum = confidenceValuesArray[0];
+    let maximumIndex = 0;
+
+    for (let i = 0; i < confidenceValuesArray.length; i++) {
+      const element = confidenceValuesArray[i];
+
+      if (element > maximum) {
+        maximum = element;
+        maximumIndex = i;
+      }
+    }
+
+    const confidenceKeysArray: any = Object.keys(classPredication.confidences);
+
+    const predictionObject = {
+      className: confidenceKeysArray[maximumIndex],
+      confidence: maximum,
+    };
+
+    setPredication(predictionObject);
   };
 
   return (
-    <Grid className="App" padding={5}>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Class Name</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={className}
-            label="Class Name"
-            onChange={handleChange}
-          >
-            <MenuItem value={"Car"}>Car</MenuItem>
-            <MenuItem value={"Bike"}>Bike</MenuItem>
-            <MenuItem value={"Fan"}>Fan</MenuItem>
-            <MenuItem value={"Mobile"}>Mobile</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <br />
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <Button variant="contained" component="label">
-          Upload
-          <input hidden accept="image/*" type="file" onChange={uploadImage} />
-        </Button>
-      </Stack>
-      {imagesUrls.length ? (
-        <Grid>
-          <ImageList sx={{ width: 500, height: 200 }} cols={3} rowHeight={164}>
-            {imagesUrls.map((item: any) => (
-              <ImageListItem key={item.img}>
-                <img
-                  src={`${item.img}`}
-                  srcSet={`${item.img}`}
-                  // alt={item.title}
-                  loading="lazy"
-                  ref={exampleImageRef}
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          <br />
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <Button
-              variant="contained"
-              component="label"
-              onClick={createExamples}
-            >
-              Create Example
-            </Button>
-          </Stack>
-        </Grid>
+    <>
+      {isModuleLoading ? (
+        "Module Is Loading"
       ) : (
-        ""
-      )}
-      <br />
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <Button variant="contained" component="label">
-          Upload Image To Identify
-          <input
-            hidden
-            accept="image/*"
-            type="file"
-            onChange={uploadImageIdentification}
-          />
-        </Button>
-      </Stack>
-      {toIdentifyImageUrls.length ? (
-        <Grid>
-          <ImageList sx={{ width: 500, height: 200 }} cols={3} rowHeight={164}>
-            {toIdentifyImageUrls.map((item: any) => (
-              <ImageListItem key={item.img}>
+        <Grid container className="App" padding={5}>
+          <Grid item xs={12}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Class Name
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={className}
+                  label="Class Name"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={"Car"}>Car</MenuItem>
+                  <MenuItem value={"Bike"}>Bike</MenuItem>
+                  <MenuItem value={"Fan"}>Fan</MenuItem>
+                  <MenuItem value={"Mobile"}>Mobile</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <br />
+          </Grid>
+          <Grid item xs={6}>
+            <Grid container>
+              <Grid item xs={6}>
+                <Stack direction="row" alignItems="center" spacing={2}>
+                  <Button variant="contained" component="label">
+                    Upload
+                    <input
+                      hidden
+                      accept="image/*"
+                      type="file"
+                      onChange={uploadImage}
+                    />
+                  </Button>
+                </Stack>
+              </Grid>
+
+              <Grid item xs={6}>
+                {selectedExampleImage && (
+                  <>
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                      <Button
+                        variant="contained"
+                        component="label"
+                        onClick={createExamples}
+                      >
+                        Create Example
+                      </Button>
+                    </Stack>
+                  </>
+                )}
+              </Grid>
+
+              <Grid item xs={6}>
+                {selectedExampleImage && (
+                  <>
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                      <img
+                        style={{ width: 200, height: 200 }}
+                        src={`${selectedExampleImage.img}`}
+                        srcSet={`${selectedExampleImage.img}`}
+                        // alt={item.title}
+                        loading="lazy"
+                        ref={exampleImageRef}
+                      />
+                    </Stack>
+                  </>
+                )}
+              </Grid>
+            </Grid>
+            <Grid item xs={6}>
+              {exampleImagesUrls.length ? (
+                <Grid>
+                  <ImageList
+                    sx={{ width: "200%", height: "200%" }}
+                    cols={3}
+                    rowHeight={164}
+                  >
+                    {exampleImagesUrls
+                      .filter((image: any) => image.class === className)
+                      .map((item: any) => (
+                        <ImageListItem key={item.img}>
+                          <img
+                            src={`${item.img}`}
+                            srcSet={`${item.img}`}
+                            // alt={item.title}
+                            loading="lazy"
+                            ref={exampleImageRef}
+                          />
+                        </ImageListItem>
+                      ))}
+                  </ImageList>
+                </Grid>
+              ) : (
+                ""
+              )}
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Button variant="contained" component="label">
+                Upload Image To Identify
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={uploadImageIdentification}
+                />
+              </Button>
+            </Stack>
+
+            {selectedIdentifyImage ? (
+              <Grid>
                 <img
-                  src={`${item.img}`}
-                  srcSet={`${item.img}`}
+                  style={{ width: 200, height: 200 }}
+                  src={`${selectedIdentifyImage.img}`}
+                  srcSet={`${selectedIdentifyImage.img}`}
                   // alt={item.title}
                   loading="lazy"
                   ref={identifyImageRef}
                 />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          <br />
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <Button variant="contained" component="label" onClick={verifyImage}>
-              Verify Images
-            </Button>
-          </Stack>
+
+                <br />
+                <Stack direction="row" alignItems="center" spacing={2}>
+                  <Button
+                    variant="contained"
+                    component="label"
+                    onClick={verifyImage}
+                  >
+                    Verify Images
+                  </Button>
+                </Stack>
+
+                {predication && (
+                  <Grid>
+                    ClassName: {predication.className}
+                    <br />
+                    Confidence: {predication.confidence}
+                  </Grid>
+                )}
+              </Grid>
+            ) : (
+              ""
+            )}
+          </Grid>
         </Grid>
-      ) : (
-        ""
       )}
-      {JSON.stringify(predication)}
-    </Grid>
+    </>
   );
 }
 
